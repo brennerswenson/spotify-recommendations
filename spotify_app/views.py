71f320016f2075520c3e6edbb916991fd6f14e6b
Brenner Swenson <brennerswenson@yahoo.com>
2018-12-29 21:06:23 +0000

    quick changes before stopping for the night

--- spotify_app/views.py
+++ spotify_app/views.py
@@ -1,17 +1,12 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
-import datetime
 # Create your views here.
 from .models import Album
 
+
 class AlbumDetailView(TemplateView):
     template_name = 'spotify_app/album_detail.html'
-
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['albums'] = Album.objects.all()
         return context
-
-
-
